945e112708d6dfe9c5beb72fe48a129fba54a052
joshikhush <khushboojoshi992@gmail.com>
2023-10-25 21:10:21 +0530

    added js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
  console.log("Welcome to spotify");
 
  let songIndex =0;
- let audioElement = new Audio('1.mp3');
+ let audioElement = new Audio('songs/1.mp3');
  let masterplay =  document.getElementById('masterplay');
  let myprogressbar = document.getElementById('myprogressbar');
  let gif = document.getElementById('gif');
+ let songitems= Array.from(document.getElementsByClassName('songitem'));
  let songs =[
-    {songName: "Tu hai toh mujhe fir or kya chahiye", filepath: "song/1.mp3", coverpath: "covers/1.jpg"},
-    {songName: "swag se  swagat", filepath: "song/s2.mp3", coverpath: "covers/3.jpg"},
-    {songName: "Kar gayi Chull", filepath: "song/k3.mp3", coverpath: "covers/4.jpg"},
-    {songName: "Balam Pichkari", filepath: "song/b2.mp3", coverpath: "covers/2.jpg"},
-    {songName: "Tere Vaaste", filepath: "song/t4.mp3", coverpath: "covers/1.jpg"},
-    {songName: "Balam Pichkari", filepath: "song/b2.mp3", coverpath: "covers/2.jpg"},
+    {songName: "Tu hai toh mujhe fir or kya chahiye", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "swag se  swagat", filePath: "songs/s2.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Kar gayi Chull", filePath: "songs/k3.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Balam Pichkari", filePath: "songs/b2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Tere Vaaste", filePath: "songs/t4.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Balam Pichkari", filePath: "songs/b2.mp3", coverPath: "covers/2.jpg"},
  ]
+ 
+ songitem.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songname")[0].innerText=songs[i].filePath;
+ })
  
  
  
